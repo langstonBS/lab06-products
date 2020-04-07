8270a6865295124aefb6279329b920c7aa8d3306
langstonBS <langston.beckwithstanley@gmail.com>
2020-04-06 17:07:53 -0700

    css is layedout for the boxes

---
+++ src/frogs.js
@@ -0,0 +1,11 @@
+
+
+
+var frog = {
+    id : 1,
+    name: 'frank',
+    image: '../assets/frog.jpeg', // './src ',
+    description:'IT a frog',
+    category: 'flying frgos', //old youg
+    price: '$100,000'//dolers
+};
