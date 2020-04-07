90798b501bfa648fccdd62a905cf931f5568dd2f
langstonBS <langston.beckwithstanley@gmail.com>
2020-04-07 14:23:30 -0700

    made data

---
+++ src/assets/checkout/cart.js
@@ -0,0 +1,20 @@
+
+
+const cart = [{
+    id: '1',
+    quantity: 2
+}, {
+    id: '3',
+    quantity: 5
+}, {
+    id: '5',
+    quantity: 1
+}, {
+    id: '6',
+    quantity: 10
+}, {
+    id: '4',
+    quantity: 10
+}];
+
+export default cart;
