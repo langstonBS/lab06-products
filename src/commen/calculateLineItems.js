7208770c70f12862654381ada804732902bd01e2
langstonBS <langston.beckwithstanley@gmail.com>
2020-04-08 13:24:08 -0700

    frist commit

--- src/commen/calculateLineItems.js
+++ src/commen/calculateLineItems.js
@@ -1,6 +1,4 @@
 
-//function calculatePrice(itemId, arryItems){
-    //takes in}
 export function findId(arryItem, id) {
         // creas a for loop and logs throg
     for (let i = 0; i < arryItem.length; i++) {
